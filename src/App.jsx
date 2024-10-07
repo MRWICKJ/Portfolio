32e218d88fbc406ca2d7ba497c61de95ee16dbfd
anuragjh <anuragjhav51@gmail.com>
2024-10-08 03:34:59 +0530

    all done

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,38 @@
 import { useState } from 'react'
 import './App.css'
 import Logo from './assets/react.svg'
-
+import Laptop from './3d_Components/Laptop/App'
 import { TimelineDemo } from './components/Timeline/app'
 import { NavbarDemo } from './components/NavBar/FixedNav'
-import Landing from './Components/Landing/app'
+import Landing from './components/Landing/app'
 import Footer from './components/Footer/App'
-import { Link } from 'react-scroll' // Import from react-scroll
-import { BentoGridDemo } from './components/Projects/App.jsx'
+import { BentoGridDemo } from './components/Projects/App'
 import Team from './components/Team/App'
-import LeetCodeProgress from './components/Leetcode/App.jsx'
-import Github from './components/Github/App.jsx'
-import GithubCal from './components/GithubCalender/App.jsx'
-import LeetcodeCal from './components/LeetcodeCalender/App.jsx'
-import SkillsSection from './components/Skills/App.jsx'
-import { TextHoverEffect } from './components/GlowText/ui.jsx'
+import LeetCodeProgress from './components/Leetcode/App'
+import Github from './components/Github/App'
+import GithubCal from './components/GithubCalender/App'
+import LeetcodeCal from './components/LeetcodeCalender/App'
+import SkillsSection from './components/Skills/App'
+import { TextHoverEffect } from './components/GlowText/ui'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
       <NavbarDemo />
-      <div id='home'>
-        <Landing />
-      </div>
+        <div id='home'>
+            <Landing/>
+        </div>
 
-      <div id='projects' className='bg-[#0a0a0a] py-10'>
+      {/* <div id='projects'>
+        <Laptop />
+      </div> */}
+      <div id='projects' className='bg-[#0a0a0a]'>
         <BentoGridDemo />
       </div>
 
-      <div id='team' className='py-10'>
+      <div id='team'>
         <Team />
       </div>
       <div id='achievements' className='bg-[#0a0a0a]'>
@@ -53,15 +57,16 @@ function App() {
         <SkillsSection />
       </div>
 
-      <div id='journey' className='py-10'>
+      <div id='journey'>
         <TimelineDemo />
       </div>
 
-      <div id='footer' className='py-10'>
+      <div id='Footer'>
         <Footer />
       </div>
+
     </>
-  );
+  )
 }
 
-export default App;
+export default App
